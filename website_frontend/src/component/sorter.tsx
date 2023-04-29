4a246f57e6c6dad9dd0f6d3bc7bb487df6dbb0b0
AdamPalmer28 <93833080+AdamPalmer28@users.noreply.github.com>
2023-04-29 10:22:49 +0100

    small updates

---
+++ website_frontend/src/component/sorter.tsx
@@ -0,0 +1,14 @@
+import Button from "react-bootstrap/Button";
+
+const NumberSorter = () => {
+	return (
+		<div className="m-3 border border-5 border-blue">
+			<span>
+				My sorter
+				<Button>Button</Button>
+			</span>
+		</div>
+	);
+};
+
+export default NumberSorter;
--- website_frontend/src/pages/home.tsx
+++ website_frontend/src/pages/home.tsx
@@ -1,9 +1,12 @@
+import NumberSorter from "../component/sorter";
+
 const Home = () => {
 	return (
 		<div>
 			<title>Home page</title>
 			<h1>Home</h1>
 			<body className="bg-primary">Body</body>
+			<NumberSorter />
 		</div>
 	);
 };
